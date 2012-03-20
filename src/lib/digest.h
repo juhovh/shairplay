268f72c87ed44283cc7d35e16198fed76353a098
Juho Vähä-Herttua <juhovh@iki.fi>
2012-03-20 22:00:14 +0200

    Fix bugs in the digest handling.

--- src/lib/digest.h
+++ src/lib/digest.h
@@ -4,6 +4,6 @@
 void digest_generate_nonce(char *result, int resultlen);
 int digest_is_valid(const char *our_realm, const char *password,
                     const char *our_nonce, const char *method,
-                    const char *authorization);
+                    const char *our_uri, const char *authorization);
 
 #endif
